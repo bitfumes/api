12a58a24cb61fe44c1652c14e5660ac1773e035c
Sarthak Shrivastava <bitfumes@gmail.com>
2017-11-08 10:26:44 +0530

    Relationship created

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -2,9 +2,13 @@
 
 namespace App\Model;
 
+use App\Model\Review;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    public function reviews()
+    {
+    	return $this->hasMany(Review::class); 
+    }
 }
--- app/Model/Review.php
+++ app/Model/Review.php
@@ -2,9 +2,13 @@
 
 namespace App\Model;
 
+use App\Model\Product;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    public function product()
+    {
+    	return $this->belongsTo(Product::class); 
+    }
 }
